d64c29d04274a3ba39840fbf8911ec13199dc703
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-11-18 08:03:29 +0000

    chore(deps): update terraform terraform-ibm-modules/resource-group/ibm to v1.1.4 (#157)

--- examples/default/pi_workspace.tf
+++ examples/default/pi_workspace.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.0"
+  version = "1.1.4"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.powervs_resource_group_name == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.powervs_resource_group_name
