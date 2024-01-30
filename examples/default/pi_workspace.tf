3424a6f6fd4a0f49c317e368bfa89a940e57e45d
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-01-30 23:32:08 +0100

    ci: update infra variation SKIP UPGRADE TEST: upgrade infra varaiton

--- examples/default/pi_workspace.tf
+++ examples/default/pi_workspace.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "powervs_infrastructure" {
   depends_on = [module.resource_group]
   source     = "terraform-ibm-modules/powervs-workspace/ibm"
-  version    = "1.2.1"
+  version    = "1.5.0"
 
   pi_zone                = var.powervs_zone
   pi_resource_group_name = local.powervs_resource_group_name
