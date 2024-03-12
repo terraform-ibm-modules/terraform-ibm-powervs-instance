f167fe5fc990487b039322c2abd40cb4ac076714
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-03-12 13:53:06 +0100

    fix: remove solution

--- examples/power-ha/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Outputs
-output "highest_pool_capacity" {
-  description = "Pool with highest storage capacity"
-  value       = data.ibm_pi_storage_pools_capacity.pools.maximum_storage_allocation.storage_pool
-}
