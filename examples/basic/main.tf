58c4618df5307b3aadf53d7123e02027004abfc6
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-06-15 01:24:34 +0000

    chore(deps): update terraform terraform-ibm-modules/resource-group/ibm to v1.1.6

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.5"
+  version = "1.1.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.powervs_resource_group_name == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.powervs_resource_group_name
