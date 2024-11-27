ca5609a1fcbcc668a2b05514cbd581b4568603ae
ludwig-mueller <ludwig.mueller@ibm.com>
2024-11-27 16:59:22 +0100

    chore: remove dependencies not used in this repo

--- modules/ansible/ansible_node_packages.sh
+++ modules/ansible/ansible_node_packages.sh
@@ -7,8 +7,8 @@
 # - updates the OS                                         #
 ############################################################
 
-GLOBAL_RHEL_PACKAGES="rhel-system-roles rhel-system-roles-sap expect"
-GLOBAL_GALAXY_COLLLECTIONS="ibm.power_linux_sap:2.1.1 ansible.utils:3.1.0 ansible.posix:1.5.4 community.general:8.4.0"
+GLOBAL_RHEL_PACKAGES="rhel-system-roles expect"
+GLOBAL_GALAXY_COLLLECTIONS="ibm.power_linux_sap:2.1.1 ansible.posix:1.5.4"
 
 ############################################################
 # Start functions
