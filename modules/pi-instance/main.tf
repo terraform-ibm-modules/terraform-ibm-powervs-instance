9a8c519ca66ab3fdd5c632d70c0554801480ea2c
Suraj Bharadwaj <101711050+surajsbharadwaj@users.noreply.github.com>
2025-09-25 14:58:34 +0200

    feat: support dedicated hosts + P11 systems (#428)

--- modules/pi-instance/main.tf
+++ modules/pi-instance/main.tf
@@ -3,18 +3,26 @@
 #####################################################
 
 resource "ibm_pi_instance" "instance" {
-  pi_cloud_instance_id     = var.pi_workspace_guid
-  pi_instance_name         = var.pi_instance_name
-  pi_image_id              = var.pi_boot_image_id
-  pi_sap_profile_id        = var.pi_sap_profile_id == null ? null : var.pi_sap_profile_id
-  pi_processors            = var.pi_sap_profile_id != null ? null : var.pi_number_of_processors
-  pi_memory                = var.pi_sap_profile_id != null ? null : var.pi_memory_size
-  pi_sys_type              = var.pi_sap_profile_id != null ? null : var.pi_server_type
-  pi_proc_type             = var.pi_sap_profile_id != null ? null : var.pi_cpu_proc_type
-  pi_key_pair_name         = var.pi_ssh_public_key_name
-  pi_health_status         = "OK"
-  pi_storage_pool_affinity = false
+  pi_cloud_instance_id = var.pi_workspace_guid
+  pi_instance_name     = var.pi_instance_name
+  pi_image_id          = var.pi_boot_image_id
+  pi_sap_profile_id    = var.pi_sap_profile_id == null ? null : var.pi_sap_profile_id
+  pi_processors        = var.pi_sap_profile_id != null ? null : var.pi_number_of_processors
+  pi_memory            = var.pi_sap_profile_id != null ? null : var.pi_memory_size
+  pi_sys_type          = var.pi_server_type
+  pi_proc_type         = var.pi_sap_profile_id != null ? null : var.pi_cpu_proc_type
+  dynamic "pi_deployment_target" {
+    for_each = length(coalesce(var.pi_deployment_target, [])) > 0 ? var.pi_deployment_target : []
+    content {
+      type = pi_deployment_target.value.type
+      id   = pi_deployment_target.value.id
+    }
+  }
 
+
+  pi_key_pair_name               = var.pi_ssh_public_key_name
+  pi_health_status               = "OK"
+  pi_storage_pool_affinity       = false
   pi_placement_group_id          = var.pi_replicants == null ? var.pi_placement_group_id : null
   pi_replicants                  = var.pi_replicants != null ? var.pi_replicants.count : null
   pi_replication_policy          = var.pi_replicants != null ? var.pi_replicants.policy : null
