c428b84ea5d9acdab4bde449d5fb86a46de6908f
ludwig-mueller <ludwig.mueller@ibm.com>
2025-10-27 12:03:19 +0100

    chore(deps): upgrade workspace to v4.0.0

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -21,7 +21,7 @@ locals {
 module "powervs_workspace" {
   depends_on = [module.resource_group]
   source     = "terraform-ibm-modules/powervs-workspace/ibm"
-  version    = "3.2.1"
+  version    = "4.0.0"
 
   pi_zone                = var.powervs_zone
   pi_resource_group_name = module.resource_group.resource_group_name
