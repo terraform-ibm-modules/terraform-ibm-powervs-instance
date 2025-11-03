8fe71fa2f0fa3d408898730278b79e48727aa321
ludwig-mueller <ludwig.mueller@ibm.com>
2025-11-03 15:00:56 +0100

    fix: make it so ansible only reinstalls modules if it detects an outdated version

--- modules/ansible/ansible_node_packages.sh
+++ modules/ansible/ansible_node_packages.sh
@@ -90,7 +90,7 @@ main::install_packages() {
       done
     done
 
-    ansible-galaxy collection install -r '/root/.ansible/collections/ansible_collections/ibm/power_linux_sap/requirements.yml' -f
+    ansible-galaxy collection install -r '/root/.ansible/collections/ansible_collections/ibm/power_linux_sap/requirements.yml' --update
     main::log_info "All packages installed successfully"
   fi
 
